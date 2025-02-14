f0bfc9f43762c6e6775da6e3f5502f0c34b45aec
Soufiane Ait naceur <sait-nac@e2r3p4.1337.ma>
2025-02-14 17:43:43 +0100

    adding fuction that handle redirections stile note here_doc

--- mandatory/execution/bonus/pipex_bonus.h
+++ mandatory/execution/bonus/pipex_bonus.h
@@ -6,38 +6,46 @@
 /*   By: sait-nac <sait-nac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 11:00:07 by sait-nac          #+#    #+#             */
-/*   Updated: 2025/02/12 17:44:17 by sait-nac         ###   ########.fr       */
+/*   Updated: 2025/02/14 15:37:44 by sait-nac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_BONUS_H
-# define PIPEX_BONUS_H
+#define PIPEX_BONUS_H
 
-# include <fcntl.h>
-# include <stdlib.h>
-# include <sys/wait.h>
-# include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-# define BUFFER_SIZE 47
+#define BUFFER_SIZE 47
 
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	**ft_split(const char *s, char c);
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s1);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char const *s1, char const *s2, char c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	**remove_slach(char *cmd);
-void	free_commands(char **commands);
-char	*check_cmd_path(char **path_list, char *cmd_name);
-char	*find_executable_path(char **env, char *cmd_name);
-char	*get_env_path(char **env);
-char	*get_next_line(int fd);
-void	ft_putstr_fd(char *s, int fd, char c);
-int		open_file(char *file, int in_or_out);
-void	executing(char **env, char *cmd_input);
-int		get_doc(char **av);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	print_error(char *cmd_input);
+typedef struct s_list {
+  char **cmd;
+  char **redirect;
+  int in_fd;
+  int out_fd;
+
+} t_list;
+
+char *ft_substr(char const *s, unsigned int start, size_t len);
+char **ft_split(const char *s, char c);
+size_t ft_strlen(const char *s);
+char *ft_strdup(const char *s1);
+char *ft_strchr(const char *s, int c);
+char *ft_strjoin(char const *s1, char const *s2, char c);
+char *ft_substr(char const *s, unsigned int start, size_t len);
+char **remove_slach(char *cmd);
+void free_commands(char **commands);
+char *check_cmd_path(char **path_list, char *cmd_name);
+char *find_executable_path(char **env, char *cmd_name);
+char *get_env_path(char **env);
+char *get_next_line(int fd);
+void ft_putstr_fd(char *s, int fd, char c);
+int open_file(char *file, int in_or_out);
+void executing(char **env, char *cmd_input);
+int get_doc(char **av);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
+void print_error(char *cmd_input);
 
 #endif
