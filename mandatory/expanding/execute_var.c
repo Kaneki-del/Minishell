f25e092db003d3a06c3e75ac755926ac9d28dd2c
bn-khalil <benkhaliltouda3201@gmail.com>
2025-03-06 12:02:27 +0000

    fix expanding hanging

--- mandatory/expanding/execute_var.c
+++ mandatory/expanding/execute_var.c
@@ -158,8 +158,8 @@ char *check_env_var(t_container *content, char *command, int *flag)
                     if (curent_part)
                         new_command = ft_strjoin(new_command, curent_part, &content->g_collector);
                 }
+                continue;
             }
-            continue;
         }
         else if(qoute != '"' && qoute != '\'' && command[i] == '~')
         {
