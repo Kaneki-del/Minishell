712781c5c25deedfbf18c55e8348939a42f2b6fe
Soufian-Aitnaceur <sofiansofianait@gmail.com>
2025-03-23 01:50:15 +0000

    after merging after the text

--- mandatory/builtins/update_pwd.d
+++
@@ -1,9 +0,0 @@
-mandatory/builtins/update_pwd.o: mandatory/builtins/update_pwd.c \
-  mandatory/builtins/../../includes/minishell.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/history.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/rlstdc.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/rltypedefs.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/readline.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/keymaps.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/chardefs.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/tilde.h
--- mandatory/utils/libft_utils1.d
+++
@@ -1,9 +0,0 @@
-mandatory/utils/libft_utils1.o: mandatory/utils/libft_utils1.c \
-  mandatory/utils/../../includes/minishell.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/history.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/rlstdc.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/rltypedefs.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/readline.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/keymaps.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/chardefs.h \
-  /mnt/homes/sait-nac/.homebrew/opt/readline/include/readline/tilde.h
